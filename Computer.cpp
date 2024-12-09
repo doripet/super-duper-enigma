09517d724896664e3a2c39b8cbe45065b4b80795
doripet <darinaajtanova@gmail.com>
2024-12-09 16:13:52 +0400

    изменение в файле

--- Computer.cpp
+++ Computer.cpp
@@ -1,6 +1,6 @@
 #include "Computer.h"
 
-// конструктор
+// конструктор ghjkl;iuytrfg
 Computer::Computer(std::string cpu_brand, double cpu_clock, std::string motherboard,
                    std::string harddrive, std::string videocard, double price, int store)
     : cpu_brand(cpu_brand), cpu_clock(cpu_clock), motherboard(motherboard),
